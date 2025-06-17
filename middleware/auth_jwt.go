9993071ab0b7872ead74a4689c6cdc4b7f9c3057
harshgupta9473 <harshharsh9473@gmail.com>
2025-06-17 12:40:03 +0530

    response in jwt

--- middleware/auth_jwt.go
+++ middleware/auth_jwt.go
@@ -3,10 +3,13 @@ package middleware
 import (
 	"context"
 	"errors"
-	"github.com/golang-jwt/jwt/v4"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/harshgupta9473/fi/utils"
 )
 
 type Claims struct {
@@ -18,11 +21,19 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenHeader := r.Header.Get("Authorization")
 		if tokenHeader == "" {
+			utils.WriteJSON(w, http.StatusUnauthorized, map[string]string{"error": "Missing Authorization header"})
 			return
 		}
-		claim, err := validateJWTToken(tokenHeader)
+		parts := strings.Split(tokenHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			utils.WriteJSON(w, http.StatusUnauthorized, map[string]string{"error": "Invalid Authorization header format"})
+			return
+		}
+		token:=parts[1]
+		claim, err := validateJWTToken(token)
 		if err != nil {
-
+			utils.WriteJSON(w, http.StatusUnauthorized, map[string]string{"error": "Invalid or expired token"})
+			return
 		}
 		ctx := context.WithValue(r.Context(), "userID", claim.UserName)
 		next.ServeHTTP(w, r.WithContext(ctx))
